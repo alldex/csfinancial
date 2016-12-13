157414a4e731e6899a1c93c566037ca42747ee62
Stephen Nielson <stephen@nielson.org>
2016-12-13 08:05:59 -0700

    Forgot one file for company commissions feature.

---
+++ affiliate-ltp/admin/class-settings.php
@@ -0,0 +1,76 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+/**
+ * Description of class-settings
+ *
+ * @author snielson
+ */
+class AffiliateLTPSettings {
+public function __construct() {
+
+		add_filter( 'affwp_settings_tabs', array( $this, 'settings_tab' ) );
+		add_filter( 'affwp_settings', array( $this, 'settings' ), 10, 1 );
+	}
+	
+	/**
+	 * Register the MLM Settings Tab
+	 *
+	 * @since 1.0
+	 */
+	public function settings_tab( $tabs ) {
+		$tabs['ltp'] = __( 'Company', 'affiliate-ltp' );
+		return $tabs;
+	}
+	
+	/**
+	 * Register MLM Settings
+	 *
+	 * @since 1.0
+	 */
+	public function settings( $settings = array() ) {
+
+		$ltp_settings = array(
+			// MLM Settings			
+			'ltp' => apply_filters( 'affwp_settings_ltp',
+				array(
+					'affwp_mlm_general_header' => array(
+						'name' => '<strong>' . __( 'General Settings', 'affiliate-ltp' ) . '</strong>',
+						'type' => 'header',
+					),
+					'affwp_ltp_company_agent_id' => array(
+						'name' => __( 'Company Agent', 'affiliate-ltp' ),
+						'desc' => '<p class="description">' . __( 'Enter an Agent ID that represents the company user.' ) . '</p>',
+						'type' => 'number',
+						'size' => 'small',
+						'std' => ''
+					),
+                                        'affwp_ltp_company_rate' => array(
+						'name' => __( 'Default Affiliate', 'affiliatewp-multi-level-marketing' ),
+						'desc' => '<p class="description">' . __( 'Enter an Affiliate ID to assign sub affiliates to a particular affiliate when no referral is found.' ) . '</p>',
+						'type' => 'number',
+						'size' => 'small',
+						'std' => ''
+					),
+					'affwp_ltp_company_rate' => array(
+						'name' => '<strong>' . __( 'Company Percentage Rate', 'affiliate-ltp' ) . '</strong>',
+						'desc' => '<p class="description">' . __( 'Enter the Company Percentage Rate that will be taken off every commission ie 5, 10, 20.' ) . '</p>',
+                                                'type' => 'number',
+						'size' => 'small',
+						'std' => ''
+					),
+				)
+			)
+		);
+
+		$settings = array_merge( $settings, $ltp_settings );
+		
+		return $settings;
+	}
+}
