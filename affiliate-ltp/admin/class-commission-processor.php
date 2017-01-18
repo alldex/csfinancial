bdc8b0593a96a73615fc955c8e5f7b209dd5fffe
Stephen Nielson <stephen@nielson.org>
2017-01-18 10:37:42 -0700

    Removed unused code.

--- affiliate-ltp/admin/class-commission-processor.php
+++ affiliate-ltp/admin/class-commission-processor.php
@@ -73,11 +73,6 @@ class Commission_Processor {
     }
     
     private function process_item(Commission_Processor_Item $item, \SplStack $processingStack) {
-        // create the commission for yourself
-        
-        // use the previous rate unless we process this one.
-        $rate = $item->previous_rate;
-        
         if ($this->should_process_item($item)) {
 
             
@@ -337,109 +332,5 @@ class Commission_Processor {
         $company_processor = new Commission_Company_Processor($this->commission_dal, $this->settings_dal);
         
         return $company_processor->create_company_commission($request);
-    }
-
-    private function create_commission($affiliateId, $amount, $reference, $directAffiliate, $paymentRate, $points, $date, $type) {
-
-        $custom = 'direct';
-        $description = __("Personal sale", "affiliate-ltp");
-        if (!$directAffiliate) {
-            $custom = 'indirect';
-            $description = __("Override", "affiliate-ltp");
-        }
-
-        $commission = array(
-            "affiliate_id" => $affiliateId
-            , "description" => $description
-            , "amount" => $amount
-            , "reference" => $reference
-            , "custom" => $custom
-            , "context" => $type
-            , "status" => self::STATUS_DEFAULT
-            , "date" => $date
-        );
-
-        // create referral
-        $commission_id = $this->commission_dal->add_commission($commission);
-        //$commission_id = affiliate_wp()->referrals->add($commission);
-
-        if ($commission_id) {
-            $this->add_commission_meta($commission_id, $paymentRate, $points);
-
-            // TODO: stephen not sure this is needed anymore... or if it is pass the array of data.
-            do_action('affwp_ltp_referral_created', $commission_id, $description, $amount, $reference, $custom);
-            return $commission_id;
-        } else {
-            // TODO: stephen add more details here.
-            throw new \Exception("Failed to create commission id for commission data: " . var_export($commission, true));
-        }
-    }
-
-    
-    private function add_commission_meta($referralId, $paymentRate, $points) {
-        // TODO: stephen should we refactor so we add in the client info here?
-        $this->commission_dal->add_commission_meta($referralId, 'agent_rate', $paymentRate);
-        $this->commission_dal->add_commission_meta($referralId, 'points', $points);
-    }
-
-    private function processAgentSplits(AffiliateLTPReferralsNewRequest $request) {
-        // if the company is taking everything we don't process anything for other
-        // agents
-        if ($request->companyHaircutAll) {
-            return;
-        }
-        
-        foreach ($request->agents as $agent) {
-            $currentAmount = $request->amount;
-            $splitPercent = $agent->split / 100;
-            $computedAmount = $request->amount * $splitPercent;
-            $request->amount = $computedAmount;
-            $this->createCommissionHeirarchy($agent->id, $computedAmount, $request);
-            $request->amount = $currentAmount; // keep amount at the same to prevent modification lower down.
-        }
-    }
-
-    private function createCommissionHeirarchy($agentId, $amount, $referralData) {
-        $reference = $referralData->client['contract_number'];
-        $points = $referralData->points;
-
-        $upline = $this->agent_dal->get_agent_upline( $agentId );
-        if ($upline) {
-            $upline = $this->agent_dal->filter_agents_by_status($upline, 'active');
-        }
-
-        $affiliates = array_merge(array($agentId), $upline);
-        
-        if ($referralData->type == AffiliateLTPCommissionType::TYPE_LIFE) {
-            $affiliates = $this->agent_dal->filter_agents_by_licensed_life_agents( $affiliates );
-        }
-        // if there are no licensed agents
-        // TODO: stephen if there are no licensed agents for this commission how do we handle that?
-        if (empty($affiliates)) {
-            return;
-        }
-        
-        $levelCount = 0;
-        $priorAffiliateRate = 0;
-
-
-        do {
-            $affiliateId = array_shift($affiliates);
-            $levelCount++;
-            $affiliateRate = $this->agent_dal->get_agent_commission_rate( $affiliateId );
-
-            $adjustedRate = ($affiliateRate > $priorAffiliateRate) ? $affiliateRate - $priorAffiliateRate : 0;
-            $adjustedAmount = $amount * $adjustedRate;
-            $priorAffiliateRate = $affiliateRate;
-
-            $directAffiliate = ($levelCount === 1);
-            $referralId = $this->create_commission($affiliateId, $adjustedAmount, $reference, $directAffiliate, $adjustedRate, $points, $referralData->date, $referralData->type);
-            $this->connectCommissionToClient($referralId, $referralData->client);
-        } while (!empty($affiliates));
-    }
-   
-     
-    private function connectCommissionToClient($referral_id, $client_data) {
-        $this->commission_dal->connect_commission_to_client( $referral_id, $client_data );
     }
 }
