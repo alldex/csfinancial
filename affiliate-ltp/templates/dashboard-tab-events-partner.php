26dd418d38f1b68a6ac001bd846436fd8529e086
Stephen Nielson <stephen@nielson.org>
2017-06-27 16:20:19 -0600

    Made the partner events similar to the company.

--- affiliate-ltp/templates/dashboard-tab-events-partner.php
+++ affiliate-ltp/templates/dashboard-tab-events-partner.php
@@ -5,17 +5,30 @@
  * All rights reserved.
  */
 ?>
-<ul>
-    <?php foreach ($events as $event) : ?>
-            <li><?= $event['title'] ?>
-                <?php if (!empty($event['registrants'])) : ?>
-                <?php 
-                    $registrants = $event['registrants']; 
-                    include 'dashboard-tab-events-registrants.php'; 
-                ?>
-                <?php else : ?>
-                <p>No one has registered yet.</p>
-                <?php endif; ?>
-            </li>
+<div class="events-accordion">
+<?php foreach ($events as $event) : ?>
+<h3><?= $event['title'] ?></h3>
+<div>
+    <?php if (!empty($event['registrants'])) : ?>
+    <table class="table table-striped affwp-table">
+        <thead>
+            <tr>
+                <th>Attendee</th>
+                <th>Spouse</th>
+                <th>Price Paid</th>
+        </thead>
+        <tbody>
+            <?php foreach ($event['registrants'] as $attendee) : ?>
+            <tr>
+                <td><?= $attendee['name']; ?></td>
+                <td><?= $attendee['spouse']; ?></td>
+                <td>$<?= $attendee['price_paid']; ?></td>
+            </tr>
+            <?php endforeach; ?>
+        </tbody>
+    </table>
+    <?php else : ?>
+        <p>No one has registered yet.</p>
+    <?php endif; ?>
+</div>
 <?php endforeach; ?>
-</ul>
